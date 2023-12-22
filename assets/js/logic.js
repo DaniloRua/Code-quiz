bcea701483fc1e4143ca75017d60d10fa26ada71
Danilopc <sr_danilo@hotmail.com>
2023-12-22 19:58:38 +0000

    timer added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,7 @@
 const startScreen = document.getElementById("start-screen");
-const startBtn = document.getElementById("start");
-
+const startBtn = document.getElementById("start")
+let timer = document.getElementById("time");
+let count = 2;
 
 //show and hide functions
 var hide = function (e) {
@@ -10,8 +11,27 @@ var show = function (e) {
     e.classList.remove("hide")
 }
 
+//start button
 startBtn.addEventListener("click", function () {
-    hide(startScreen)
+    hide(startScreen);
+    startTimer();
     show(questions)
 })
 
+//timer
+function startTimer() {
+
+    const time = setInterval(function () {
+    count--;
+    timer.innerHTML = count
+
+//penalties...
+
+        if (count === 0) {
+            clearInterval(time)
+    
+        }
+    }, 1000);
+
+
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,12 @@
+const questionsAndAnswers = [
+    {
+        question: "How to declare a variable in JavaScript?",
+        answers: ["variable", "v","var","vet"],
+        correctIndex: 3
+      },
+      {
+        question: "What does the 'DOM' stand for in JavaScript?",
+        answers: ["Document Object Model", "Data Object Model", "Document Oriented Model", "Dynamic Object Model"],
+        correctIndex: 0
+      }
+    ]
