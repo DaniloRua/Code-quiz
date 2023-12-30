36cda3792722431f027275ef6b671dc8a0fa6405
Danilopc <sr_danilo@hotmail.com>
2023-12-30 20:34:51 +0000

    question populated added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,12 @@
 const startScreen = document.getElementById("start-screen");
 const startBtn = document.getElementById("start")
+const questions = document.getElementById("questions");
+let questionTitle = document.getElementById("question-title");
+let choices = document.getElementById("choices");
 let timer = document.getElementById("time");
-let count = 2;
+const endScreen = document.getElementById("end-screen")
+let count = 75;
+
 
 //show and hide functions
 var hide = function (e) {
@@ -16,22 +21,44 @@ startBtn.addEventListener("click", function () {
     hide(startScreen);
     startTimer();
     show(questions)
+    getQuestions()
 })
 
 //timer
 function startTimer() {
 
     const time = setInterval(function () {
-    count--;
-    timer.innerHTML = count
+        count--;
+        timer.innerHTML = count
 
-//penalties...
+        //penalties...
 
         if (count === 0) {
             clearInterval(time)
-    
+            show(endScreen)
+            hide(questions)
         }
     }, 1000);
 
-
 }
+
+
+
+//populated the questions
+function getQuestions(){
+
+
+    questionTitle.textContent = questionsAndAnswers[0].question
+    let answersContainer = questionsAndAnswers[0].answers
+    
+    for (let i = 0; i < answersContainer.length; i++) {
+        let number = i
+
+        var button = document.createElement("button")
+        button.textContent = number+1 + ". " + answersContainer[i]
+        choices.appendChild(button)
+    }
+   
+   
+}
+
